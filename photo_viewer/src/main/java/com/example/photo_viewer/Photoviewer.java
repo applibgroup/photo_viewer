e7311487386ffd8b1025186acc64f75c66445bfc
venkatasai <venkatasaipolu9999@gmail.com>
2022-01-18 10:03:09 +0530

    SDK Version Changed

--- photo_viewer/src/main/java/com/example/photo_viewer/Photoviewer.java
+++ photo_viewer/src/main/java/com/example/photo_viewer/Photoviewer.java
@@ -1,3 +1,18 @@
+/*
+        Copyright (C) 2020-21 Application Library Engineering Group
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
 package com.example.photo_viewer;
 import ohos.agp.animation.AnimatorProperty;
 import ohos.agp.components.AttrSet;
@@ -7,7 +22,7 @@ import ohos.app.Context;
 import java.util.Timer;
 import java.util.TimerTask;
 public class Photoviewer extends Image implements Component.ClickedListener, ohos.agp.render.render3d.Component {
-        public Component component;
+       public Component component;
         public Photoviewer(Context context, AttrSet attrSet) {
                 super(context, attrSet);
                 setClickedListener(this);
